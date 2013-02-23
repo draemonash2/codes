e0209e83e65b894e80739c23807e7784764abc5b
Tatsuya Endo <draemon_ash3@yahoo.co.jp>
2013-02-23 09:52:26 +0900

    add new bat

--- bat/001_Dig_ToDoAtShutDown.bat
+++ bat/001_Dig_ToDoAtShutDown.bat
@@ -1,9 +1,17 @@
 @echo off
+call lib\010_Def_Datetime.bat
 
 set LOGDIR=%~n0.log
 
+echo Execute date is %date% %time% >> %LOGDIR%
+echo {{{ >> %LOGDIR%
+
 echo ######### Digest ToDoAtShutDown! #########
 echo ####        Wait for a while ...      ####
+echo %date% %time% >> %LOGDIR%
 ruby ..\ruby\dig_ToDoAtShutDown.rb >> %LOGDIR%
 echo ############### Finish! ##################
 echo.
+
+echo }}} >> %LOGDIR%
+echo. >> %LOGDIR%
