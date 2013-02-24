bccf3ed01e749285c02562feb95cf2614c424b39
Tatsuya Endo <draemon_ash3@yahoo.co.jp>
2013-02-24 18:10:53 +0900

    add datetime

--- bat/010_Syn_Drive.bat
+++ bat/010_Syn_Drive.bat
@@ -1,12 +1,19 @@
 @echo off
-
+call lib\010_Def_Datetime.bat
+	
 set LOGDIR=%~n0.log
+
+echo Execute date is %date% %time% >> %LOGDIR%
+echo {{{ >> %LOGDIR%
 
 echo ############## Sync Drives! ##############
 set /p SRC="### Source      Path [ex. D:\] : "
 set /p DST="### Destination Path [ex. E:\] : "
-pause
+set /p ANS="### Please press any key ..."
 echo ### Wait for a while ...
 robocopy %SRC% %DST% /MIR >> %LOGDIR%
 echo ############### Finish! ##################
 pause
+
+echo }}} >> %LOGDIR%
+echo. >> %LOGDIR%
