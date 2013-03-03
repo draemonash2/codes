32e68f0a68357a470f96a9bb684d6e4713f34f33
Tatsuya Endo <draemon_ash3@yahoo.co.jp>
2013-03-03 11:27:57 +0900

    update syn drive

--- bat/010_Syn_Drive.bat
+++ bat/010_Syn_Drive.bat
@@ -6,7 +6,7 @@ set LOGDIR=.\log\%~n0_%datetime%.log
 set SRC=D:\
 set DST=E:\
 
-echo ############# Sync Directry! #############
+echo ############### Sync Drive! ##############
 echo ### Source      Path is %SRC%
 echo ### Destination Path is %DST%
 set /p ANS="### Please press any key ..."
