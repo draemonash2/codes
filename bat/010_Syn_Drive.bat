7493726280eccb2274de689b5677a06181b4a65f
tatsuya endo <draemon_ash3@yahoo.co.jp>
2016-10-12 15:44:06 +0900

    update sync scripts

--- bat/010_Syn_Drive.bat
+++ bat/010_Syn_Drive.bat
@@ -1,18 +1,19 @@
 @echo off
 call lib\010_Def_Datetime.bat
 
-set LOGDIR=X:\101_BackupFile_log\%~n0_%datetime%.log
-
 set SRC=Z:\
-set DST=X:\100_BackupFile
+set DSTBASE=X:\100_BackUp_Data
+
+set DST=%DSTBASE%\data
+set LOG=%DSTBASE%\log\%~n0_%datetime%.log
 
 echo ############### Sync Drive! ##############
 echo ### Source      Path is %SRC%
 echo ### Destination Path is %DST%
 set /p ANS="### Please press any key ..."
 echo ### Wait for a while ...
-robocopy %SRC% %DST% /MIR /XD "System Volume Information" >> %LOGDIR%
+robocopy %SRC% %DST% /MIR /XD "System Volume Information" >> %LOG%
 echo ############### Finish! ##################
 pause
 
-echo. >> %LOGDIR%
+echo. >> %LOG%
--- bat/012_Syn_Dropbox.bat
+++ bat/012_Syn_Dropbox.bat
@@ -1,37 +1,19 @@
-:: ==============================================
-:: <<実行時の注意>>
-::   本バッチファイル実行前に、
-::     WScript.Quit(WeekDay(Date))
-::   と記述された VBS ファイルが C:\Windows 配下に
-::   格納されていることを確認する。
-::   存在しない場合は、格納しておく。
-:: ==============================================
-
 @echo off
+call lib\010_Def_Datetime.bat
 
-cscript /b C:\Windows\CheckWeekDay.vbs
+set SRC=C:\Users\draem_000\Documents\Dropbox
+set DSTBASE=X:\120_BackUp_Dropbox
 
-:: 月曜日のみ実行
-if not %errorlevel% == 2 goto :END
+set DST=%DSTBASE%\data
+set LOG=%DSTBASE%\log\%~n0_%datetime%.log
 
-	call lib\010_Def_Datetime.bat
+echo ############### Sync Drive! ##############
+echo ### Source      Path is %SRC%
+echo ### Destination Path is %DST%
+set /p ANS="### Please press any key ..."
+echo ### Wait for a while ...
+robocopy %SRC% %DST% /MIR /XD "System Volume Information" >> %LOG%
+echo ############### Finish! ##################
+pause
 
-	set LOGDIR=.\log\%~n0_%datetime%.log
-
-	set SRC=C:\Users\TatsuyaEndo\Dropbox
-	set DST=Z:\100_Documents\100_PC\90_BackUp\Dropbox
-
-	echo ############### Sync Drive! ##############
-	echo ### Source      Path is %SRC%
-	echo ### Destination Path is %DST%
-	set /p ANS="### Please press any key ..."
-	echo ### Wait for a while ...
-	echo {{{ >> %LOGDIR%
-	robocopy %SRC% %DST% /MIR >> %LOGDIR%
-	echo }}} >> %LOGDIR%
-	echo ############### Finish! ##################
-	pause
-
-	echo. >> %LOGDIR%
-
-:END
+echo. >> %LOG%
