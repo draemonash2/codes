06f30eeb4b9a7c73376307e5c4881d14cb664227
tatsuya endo <draemon_ash3@yahoo.co.jp>
2016-11-07 19:03:05 +0900

    modified log output path

--- bat/011_Syn_Library.bat
+++ bat/011_Syn_Library.bat
@@ -1,23 +1,17 @@
 @echo off
-if {%MYPATH_CODE_BAT%} == {0} (
-	echo target environment variable is nothing!
-	pause
-	exit /B 0
-)
-call %MYPATH_CODE_BAT%\lib\010_Def_Datetime.bat
 
 set SRC=Z:\
-set DSTBASE=X:\800_BackUp_Library
+set DSTBASE=\\RASPBERRYPI\pockethdd\800_BackUp_Library
 
 set DST=%DSTBASE%\data
-set LOG=%DSTBASE%\log\%~n0_%datetime%.log
+set LOG=%DSTBASE%\%~n0.log
 
 echo ############### Sync Drive! ##############
 echo ### Source      Path is %SRC%
 echo ### Destination Path is %DST%
 set /p ANS="### Please press any key ..."
 echo ### Wait for a while ...
-robocopy %SRC% %DST% /MIR /XD "System Volume Information" >> %LOG%
+robocopy %SRC% %DST% /MIR /XD "System Volume Information" > %LOG%
 echo ############### Finish! ##################
 pause
 
--- bat/012_Syn_Dropbox_Auto.bat
+++ bat/012_Syn_Dropbox_Auto.bat
@@ -1,22 +1,16 @@
 @echo off
-if {%MYPATH_CODE_BAT%} == {0} (
-	echo target environment variable is nothing!
-	pause
-	exit /B 0
-)
-call %MYPATH_CODE_BAT%\lib\010_Def_Datetime.bat
 
 set SRC=C:\Users\draem_000\Documents\Dropbox
 set DSTBASE=\\RASPBERRYPI\pockethdd\820_BackUp_Dropbox
 
 set DST=%DSTBASE%\data
-set LOG=%DSTBASE%\log\%~n0_%datetime%.log
+set LOG=%DSTBASE%\%~n0.log
 
 echo ############### Sync Drive! ##############
 echo ### Source      Path is %SRC%
 echo ### Destination Path is %DST%
 echo ### Wait for a while ...
-robocopy %SRC% %DST% /MIR /XD "System Volume Information" >> %LOG%
+robocopy %SRC% %DST% /MIR /XD "System Volume Information" > %LOG%
 echo ############### Finish! ##################
 
 echo. >> %LOG%
