f7303c3118e2f1eb1513f3ad68220dcbd6400d8a
tatsuya endo <draemon_ash3@yahoo.co.jp>
2016-11-27 20:21:17 +0900

    delete debug code

--- bat/lib/DeleteEmptyFolders.bat
+++ bat/lib/DeleteEmptyFolders.bat
@@ -10,7 +10,7 @@ setlocal
 if "%~1" == "" goto negative
 
 set ARG=%~1
-echo argument : %ARG%
+::	echo argument : %ARG%
 
 ::––”ö‚É \ ‚ª•t—^‚³‚ê‚Ä‚¢‚éê‡A\ ‚ðíœ‚µ‚Ä‚â‚è’¼‚µ
 if %ARG:~-1% == \ (
@@ -23,22 +23,21 @@ if %TRGT_DIR_PATH_TMP:~-1% == \ (
 ) else (
 	set TRGT_DIR_PATH=%TRGT_DIR_PATH_TMP%
 )
-echo target dir     : %TRGT_DIR_PATH%
+::	echo target dir     : %TRGT_DIR_PATH%
 if not exist "%TRGT_DIR_PATH%" goto negative
 
 set TRGT_PAR_DIR_PATH_TMP=%~dp1
 set TRGT_PAR_DIR_PATH=%TRGT_PAR_DIR_PATH_TMP:~0,-1%
-echo target par dir : %TRGT_PAR_DIR_PATH%
+::	echo target par dir : %TRGT_PAR_DIR_PATH%
 
 :positive
 set FILE_EXISTS=FALSE
 for    %%i in ("%TRGT_DIR_PATH%\*") do set FILE_EXISTS=TRUE
 for /d %%i in ("%TRGT_DIR_PATH%\*") do set FILE_EXISTS=TRUE
 if %FILE_EXISTS% == FALSE (
-	rmdir /s /q "%TRGT_DIR_PATH%"
+	rmdir /s /q "%TRGT_DIR_PATH%" >NUL 2>&1
 	call %0 "%TRGT_PAR_DIR_PATH%"
 ) else (
-	echo target dir is stay.
 	echo TRUE
 )
 goto end
@@ -48,6 +47,3 @@ echo FALSE
 
 :end
 endlocal
-
-
-::	rmdir /s /q "%TRGT_DIR_PATH%" >NUL 2>&1
