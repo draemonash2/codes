d67f88f8bad63619f73d2923b6b0dda193fbae9d
tatsuya endo <draemon_ash3@yahoo.co.jp>
2016-11-28 23:46:05 +0900

    - if not execution at runas mode, stop to execute - use log file option

--- bat/010_Syn_Directry.bat
+++ bat/010_Syn_Directry.bat
@@ -1,14 +1,24 @@
 @echo off
 
-set LOGDIR=%~dp0%~n0.log
+set LOG=%~dp0%~n0.log
+set OPT=
+set OPT=%OPT% /MIR
+set OPT=%OPT% /SL
+set OPT=%OPT% /XD "System Volume Information"
+set OPT=%OPT% /LOG:%LOG%
+
+whoami /PRIV | FIND "SeLoadDriverPrivilege" > NUL
+if errorlevel 1 (
+	echo ### result : [error  ] please execute on runas mode!
+	pause
+	exit /B 0
+)
 
 echo ############# Sync Directry! #############
 set /p SRC="### Source      Path [ex. D:\] : "
 set /p DST="### Destination Path [ex. E:\] : "
 set /p ANS="### Please press any key ..."
 echo ### Wait for a while ...
-robocopy %SRC% %DST% /MIR >> %LOGDIR%
+robocopy %SRC% %DST% %OPT% >NUL 2>&1
 echo ############### Finish! ##################
 pause
-
-echo. >> %LOGDIR%
--- bat/013_Syn_GoogleDrive.bat
+++ bat/013_Syn_GoogleDrive.bat
@@ -2,17 +2,27 @@
 
 set SRC=C:\Users\draem_000\Documents\GoogleDrive
 set DSTBASE=\\RASPBERRYPI\pockethdd\830_BackUp_GoogleDrive
-
 set DST=%DSTBASE%\data
 set LOG=%DSTBASE%\%~n0.log
+set OPT=
+set OPT=%OPT% /MIR
+set OPT=%OPT% /SL
+set OPT=%OPT% /XF "Current Session"
+set OPT=%OPT% /XF "Current Tabs"
+set OPT=%OPT% /LOG:%LOG%
+
+whoami /PRIV | FIND "SeLoadDriverPrivilege" > NUL
+if errorlevel 1 (
+	echo ### result : [error  ] please execute on runas mode!
+	pause
+	exit /B 0
+)
 
 echo ############### Sync Drive! ##############
 echo ### Source      Path is %SRC%
 echo ### Destination Path is %DST%
 set /p ANS="### Please press any key ..."
 echo ### Wait for a while ...
-robocopy %SRC% %DST% /MIR /XD "System Volume Information" > %LOG%
+robocopy %SRC% %DST% %OPT% >NUL 2>&1
 echo ############### Finish! ##################
 pause
-
-echo. >> %LOG%
