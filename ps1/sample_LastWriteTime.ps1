08f052a5bfcd5a1de9b485fa9d4de4b0b696acb7
tatsuya endo <draemon_ash3@yahoo.co.jp>
2016-12-15 03:02:14 +0900

    add

---
+++ ps1/sample_LastWriteTime.ps1
@@ -0,0 +1,2 @@
+Set-ItemProperty -path "W:\810_BackUp_iTunes\MobileSync\Backup\90cda35377b21eaa0043fd0a110c7bc7f45871dc\00" -Name LastWriteTime -Value "2016/6/15 4:25:12"
+Set-ItemProperty -path "W:\810_BackUp_iTunes\MobileSync\Backup\90cda35377b21eaa0043fd0a110c7bc7f45871dc\Info.plist" -Name LastWriteTime -Value "2015/8/25 9:34:14"
