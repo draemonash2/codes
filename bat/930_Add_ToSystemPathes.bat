0cda5dfb6c04c50269d4042f3bfc51a4f73c2e66
tatsuya endo <draemon_ash3@yahoo.co.jp>
2017-05-01 21:46:25 +0900

    modified

--- bat/930_Add_ToSystemPathes.bat
+++ bat/930_Add_ToSystemPathes.bat
@@ -1,25 +1,24 @@
 @echo off
 ::	<<概要>>
-::	  TARGET_DIR_PATH 配下（サブフォルダ含む）にあるファイル
-::	  「_add_to_path.bat」を探し、それが格納されたフォルダパスを
+::	  TARGET_DIR_PATH 配下（サブフォルダ含む）にあるファイル名
+::	  KEY_FILE_NAME を探し、それが格納されたフォルダパスを
 ::	   システム環境変数の「Path」に追加する
 ::	
 ::	<<使い方>>
-::	  １．以下のファイルを作成
-::			ファイル名：_add_to_path.bat
-::			ファイルの中身：call %1 %~dp0
-::	  ２．１で作成したファイルを Path に追加したいフォルダ内にコピーする
-::	  ３．本バッチファイルを「管理者として実行」する
+::	  １．KEY_FILE_NAME に指定した名前のファイルを、システム環境変数の
+::		  「Path」に追加したいフォルダ内にコピーする
+::	  ２．本バッチファイルを「管理者として実行」する
 
 :: ### 設定情報 ###
 set ADD_TO_PATH_SCRIPT_PATH=C:\codes\vbs\700_AddToPathOfEnvVariable.vbs
 set TARGET_DIR_PATH=C:\prg_exe\
+set KEY_FILE_NAME=_add_to_sys_env_directory
 
 :: ### 処理 ###
-FOR /R "%TARGET_DIR_PATH%" %%i IN (_add_to_path.bat) DO (
+FOR /R "%TARGET_DIR_PATH%" %%i IN (%KEY_FILE_NAME%) DO (
 	if exist %%i (
-		echo %%i
-		call %%i %ADD_TO_PATH_SCRIPT_PATH%
+		echo %%~dpi
+		call %ADD_TO_PATH_SCRIPT_PATH% %%~dpi
 	)
 )
 pause
