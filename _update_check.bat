f576e76400501b6c5e6c146c70abf65310b868c4
draemonash2 <draemon_ash3@yahoo.co.jp>
2021-05-11 11:28:46 +0900

    mod

--- _update_check.bat
+++ _update_check.bat
@@ -1,5 +1,4 @@
-echo フォルダをダウンロードして比較します。
+echo githubからダウンロードして比較します。
 "%MYDIRPATH_CODES%\vbs\tools\win\file_ope\UpdateCheck.vbs" "%~dp0" "https://github.com/draemonash2/codes/archive/master.zip" "codes-master"
 echo %MYDIRPATH_CODES%内の_localフォルダを比較します。
 "%MYDIRPATH_CODES%\vbs\tools\win\file_ope\DiffLocalDirs.vbs" "%~dp0"
-
