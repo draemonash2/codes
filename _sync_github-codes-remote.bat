6b30762a0973e01899b6d3fabee25eb44b732390
draemonash2 <draemon_ash3@yahoo.co.jp>
2022-11-23 16:54:52 +0900

    mod

--- _sync_github-codes-remote.bat
+++ _sync_github-codes-remote.bat
@@ -6,8 +6,13 @@ for /f "tokens=1,2,3,4" %%a in (%MYDIRPATH_CODES_CONFIG%\_sync_github-codes-remo
 )
 
 echo githubからダウンロードして比較します。
+pause
 "%MYDIRPATH_CODES%\vbs\tools\win\file_ope\UpdateCheck.vbs" "%MYDIRPATH_CODES%" "https://github.com/draemonash2/codes/archive/master.zip" "codes-master"
+
 echo %MYDIRPATH_CODES%内の_localフォルダを比較します。
+pause
 "%MYDIRPATH_CODES%\vbs\tools\win\file_ope\DiffLocalDirs.vbs" "%MYDIRPATH_CODES%"
+
 echo %MYDIRPATH_CODES%とremote接続先のファイルを比較します。
+pause
 "%MYDIRPATH_CODES%\vbs\tools\win\file_ope\DiffLclVsRmt.vbs" "%USER%" "%PASSWORD%" "%HOST%" "%HOMEDIR%"
