2b85d8f49e9d9f600b234049c7af35541dc374d8
Tatsuya Endo <draemon_ash3@yahoo.co.jp>
2023-07-08 11:13:25 +0900

    mod

---
+++ python/_tp/cmn/tp.py
@@ -0,0 +1,8 @@
+position = ("123-4567", "東京都・・・", 35.686321, 139.782211)
+print(position[0:2])
+print(len(position))
+yuubin, address, latitude, longitude = position
+print(yuubin)
+print(address)
+print(latitude)
+print(longitude)
