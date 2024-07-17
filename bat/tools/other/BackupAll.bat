c8b45c958ce4598af883f9cae6e251cab2932725
Tatsuya Endo <draemon_ash3@yahoo.co.jp>
2024-07-17 16:34:53 +0900

    mod

--- bat/tools/other/BackupAll.bat
+++ bat/tools/other/BackupAll.bat
@@ -2,9 +2,11 @@
 
 set CUR_DIR=%~dp0
 	call "%CUR_DIR%\BackupPrograms.bat"
+	call "%CUR_DIR%\BackupPrgExe.bat"
 	call "%CUR_DIR%\BackupCodesSample.bat"
 	call "%CUR_DIR%\BackupLibrary.bat"
 	call "%CUR_DIR%\BackupRoot.bat"
+	call "%CUR_DIR%\BackupSSHKey.bat"
 
 pause
 
